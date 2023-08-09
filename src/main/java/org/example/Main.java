3c2092f21191bf88160a9e81bbb872d62cff9645
zamirlazarov1 <94539113+zamirlazarov1@users.noreply.github.com>
2023-08-09 17:09:47 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This is my third push!");
+        System.out.println("This is my new branch!");
     }
 }
